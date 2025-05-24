6d38ae7c9b6a1bed7e9bf679383c962db97beb1b
fairygupta04 <fairygupta04@gmail.com>
2025-05-24 18:43:12 +0530

    Update two_sum.cpp

--- hashmap/two_sum.cpp
+++ hashmap/two_sum.cpp
@@ -1,6 +1,11 @@
 // Problem: Two Sum
 // Topic: HashMap
 // Link: https://leetcode.com/problems/two-sum/
+//Approach:
+//->Use a map to store number -> index pairs.
+//->For each element, check if (target - current element) exists in map.
+//->If it exists, return indices as the solution.
+//->Otherwise, add current element and index to the map and continue.
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
